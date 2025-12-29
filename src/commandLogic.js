f12df97a90365f88ae62bdc355faec4d5fb66267
ScottishDex <35346954+ScotDex@users.noreply.github.com>
2025-12-29 15:23:31 +0000

    removed divider

--- src/commandLogic.js
+++ src/commandLogic.js
@@ -61,7 +61,6 @@ async function handleRoutes(interaction, systemName) {
                 `**Routes:** ${outLink} ↔ ${inLink}`,
                 `**Signatures:** \`${sig.out_signature}\` ↔ \`${sig.in_signature}\``,
                 `**Class:** \`${sig.in_system_class}\` | **Expires:** <t:${unixExpiry}:R>`,
-                `───────────────────────────────────────────────────`
             ].join('\n'),
             inline: true
         });
